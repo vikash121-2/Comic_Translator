6179c54c161b9963d3694398cd09e52f874fee5e
vikash121-2 <vikash121929@gmail.com>
2025-09-16 16:56:24 +0530

    Update comic_bot.py

--- comic_bot.py
+++ comic_bot.py
@@ -41,8 +41,8 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 # --- Global Variables & Configuration ---
-BOT_TOKEN = "YOUR_BOT_TOKEN_HERE"  # <-- IMPORTANT: Replace with your bot token
-FONT_PATH = "font.ttf"  # <-- IMPORTANT: Make sure this font file is in the same directory
+BOT_TOKEN = "6298615623:AAEyldSFqE2HT-2vhITBmZ9lQL23C0fu-Ao"  # <-- IMPORTANT: Replace with your bot token
+FONT_PATH = "DMSerifText-Regular.ttf"  # <-- IMPORTANT: Make sure this font file is in the same directory
 
 # Conversation states
 (
